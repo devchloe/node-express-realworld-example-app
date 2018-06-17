64ca936efe97753f4d2a237a533f4fb27cb21528
Chloe Kwon <yoonjeong1104@gmail.com>
2018-06-17 15:14:58 +0900

    make endpoint for retrieving article by slug

--- routes/api/articles.js
+++ routes/api/articles.js
@@ -20,7 +20,27 @@ router.post('/', auth.required, function(req, res, next) {
     }).catch(next);
 })
 
+router.param('article', function(req, res, next, slug){
+    Article.findOne({slug: slug})
+        .populate('author')
+        .then(function(article) {
+            if(!article) { return res.sendStatus(404); }
 
+            req.article = article;
 
+            return next();
+    }).catch(next);
+});
+
+router.get('/:article', auth.optional, function(req, res, next) {
+    Promise.all([
+        req.payload ? User.findById(req.payload.id) : null,
+        req.article.populate('author').execPopulate()
+    ]).then(function(results) {
+        var user = results[0];
+
+        return res.json({article: req.article.toJSONFor(user)});
+    }).catch(next);
+});
 
 module.exports = router;
