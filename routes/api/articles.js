55915818beaf79a52a0c1f66161e41fe1a0416cd
Chloe Kwon <yoonjeong1104@gmail.com>
2018-06-17 17:04:11 +0900

    create endpoint for favoriting article

--- routes/api/articles.js
+++ routes/api/articles.js
@@ -81,4 +81,19 @@ router.delete('/:article', auth.required, function(req, res, next) {
     });
 });
 
+router.post('/:article/favorite', auth.required, function(req, res, next) {
+    var articleId = this.article._id;
+
+    User.findById(req.payload.id).then(function(user) {
+        if (!user) { return res.sendStatus(401); }
+
+        return user.favorite(articleId).then(function() {
+            return req.article.updateFavoriteCount().then(function(article) {
+                return res.json({article: article.toJSONFor(user)});
+            });
+        });
+    }).catch(next);
+
+});
+
 module.exports = router;
