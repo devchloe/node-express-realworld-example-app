f6872ebed9ea9338799817f10efdd37221cba9bf
Chloe Kwon <yoonjeong1104@gmail.com>
2018-06-26 20:58:04 +0900

    [queryable endpoint]
    - create endpoint to list all articles with limit, offset
    - add to filter articles by tags
    - add to filter articles by author and favoriter

--- routes/api/articles.js
+++ routes/api/articles.js
@@ -172,4 +172,62 @@ router.delete('/:article/comments/:comment', auth.required, function(req, res, n
     }
 });
 
+router.get('/', auth.optional, function(req, res, next) {
+    var query = {};
+    var limit = 20;
+    var offset = 0;
+
+    if(typeof req.query.limit !== 'undefined') {
+        limit = req.query.limit;
+    }
+
+    if(typeof req.query.offset !== 'undefined') {
+        offset = req.query.offset;
+    }
+
+    if(typeof req.query.tag !== 'undefined') {
+        query.tagList = {"$in" : [req.query.tag]};
+    }
+
+    Promise.all([
+        req.query.author ? User.findOne({username: req.query.author}) : null,
+        req.query.favorited ? User.findOne({username: req.query.favorited}) : null
+    ]).then(function(results) {
+        var author = results[0];
+        var favoriter = results[1];
+
+        if(author) {
+            query.author = author._id;
+        }
+
+        if(favoriter) {
+            query._id = {$in: favoriter.favorites};
+        } else if(req.query.favorited) {
+            query._id = {$in: []};
+        }
+
+        return Promise.all([
+            Article.find(query)
+                .limit(Number(limit))
+                .skip(Number(offset))
+                .sort({createdAt: 'desc'})
+                .populate('author')
+                .exec(),
+            Article.updateFavoriteCount(query).exec(),
+            req.payload ? User.findById(req.payload.id) : null,
+        ]).then(function(results) {
+            var articles = results[0];
+            var articlesCount = results[1];
+            var user = results[2];
+
+            return res.json({
+                articles: articles.map(function(article) {
+                    return article.toJSONFor(user);
+                }),
+                articlesCount: articlesCount
+            });
+        });
+    }).catch(next);
+});
+
 module.exports = router;
